43172314b7919bde336eac0f6c2d4759285c1b37
Petter Graff <petter.graff@scispike.com>
2015-09-27 10:20:36 -0500

    Fixed a most likely dead solution for the interface

--- interfacesSol/main.cpp
+++ interfacesSol/main.cpp
@@ -3,26 +3,26 @@
 
 template <class X, int howManyTimes>
 void callConstructorManyTimes() {
-	for (int i = 0; i < howManyTimes; i++) {
-		X * x = new X;
-		delete x;
-	}
+  for (int i = 0; i < howManyTimes; i++) {
+    X * x = new X;
+    delete x;
+  }
 }
 
 
-void main() {
-	
-	const int numberOfTimes = 100000;
-	/*
-	callConstructorManyTimes<DoubleSidedPrinter, numberOfTimes>();
-	callConstructorManyTimes<MyDoubleSidedPrinter, numberOfTimes>();
-	*/
-	
-	for (int i = 0; i < numberOfTimes; i++) {
-		DoubleSidedPrinter * x = new DoubleSidedPrinter;
-		delete x;
-		MyDoubleSidedPrinter * y = new MyDoubleSidedPrinter;
-		delete y;
-	}
-	
+int main() {
+  
+  const int numberOfTimes = 100000;
+  /*
+    callConstructorManyTimes<DoubleSidedPrinter, numberOfTimes>();
+    callConstructorManyTimes<MyDoubleSidedPrinter, numberOfTimes>();
+  */
+  
+  for (int i = 0; i < numberOfTimes; i++) {
+    DoubleSidedPrinter * x = new DoubleSidedPrinter;
+    delete x;
+    MyDoubleSidedPrinter * y = new MyDoubleSidedPrinter;
+    delete y;
+  }
+  return 0;
 }
